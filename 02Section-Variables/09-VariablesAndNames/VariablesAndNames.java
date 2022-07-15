55fe0c432f3db6c5e090c30c7e23256b09fc482a
Michael "Cal" Krug <cal.krug@proton.me>
2022-07-15 11:33:01 -0500

    Add: Section 2, Problem 9 | Variables and Names

--- 02Section-Variables/09-VariablesAndNames/VariablesAndNames.java
+++ 02Section-Variables/09-VariablesAndNames/VariablesAndNames.java
@@ -10,14 +10,22 @@ public class VariablesAndNames {
 		int cars, drivers, passengers, cars_not_driven, cars_driven;
 		double space_in_a_car, carpool_capacity, average_passengers_per_car;
 
+		// Variable assignment of an integer value of "100" to the variable "cars"
 		cars = 100;
+		// Variable assignment of a floating point value of "4.0" to the variable "space_in_a_car"
 		space_in_a_car = 4.0;
+		// assign int "30" to the var "cars"
 		drivers = 30;
+		// assign int "90" to the var "passengers"
 		passengers = 90;
-		cars_not_driven = cars - drivers; //by default, the pure value is "70"
+		// assign value of cars (100) negative drivers (30) to the var "cars_not_driven" (70)
+		cars_not_driven = cars - drivers;
+		// assign val drivers (30) to the var "cars_driven"
 		cars_driven = drivers;
-		carpool_capacity = cars_driven * space_in_a_car; // default, pure value is "120.0"
-		average_passengers_per_car = passengers / cars_driven; // pure value is "3"
+		// assign val cars_driven (drivers, 30) times space_in_a_car (4.0) to the var "cars" (120.0)
+		carpool_capacity = cars_driven * space_in_a_car;
+		// assign val passengers (90) divided by cars_driven (drivers, 30) to the var "cars" (3)
+		average_passengers_per_car = passengers / cars_driven;
 
 		System.out.println("There are " + cars + " cars available.");
 		System.out.println("There are only " + drivers + " drivers available.");
@@ -36,7 +44,9 @@ public class VariablesAndNames {
 
     2. Remember that 4.0 is a "floating point" number. Find out what that means.
 
-	
+	Floats represent a bypass in how computers represent non-integer, decimal numbers, (i.e. "floating point representation")
+	with "floating" referring to how the radix point (or radix character, or the "dot" ".") can more higher or lower when
+	multipled by a exponent (power).
 
     3. Write comments above each of the variable assignments.
     4. Make sure you know what = is called (equals) and that it's making names for things.
