6f4f340cf8d4d4a69ff7dc72b85c3cc424358e90
Michael "Cal" Krug <cal.krug@proton.me>
2022-08-16 22:48:00 -0500

    Add: Section 3 - BMI Calculator

--- 03Section-KeyboardInput/20-BMICalculator/BMICalculator.java
+++ 03Section-KeyboardInput/20-BMICalculator/BMICalculator.java
@@ -1,13 +1,18 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class BMICalculator {
 	public static void main (String args[]) {
 
 		Scanner keyboard = new Scanner(System.in);
 
+		float userHeight, userWeight, userBMI;
+
 		System.out.println("Your height in m: ");
-		keyboard.nextLine();
+		userHeight = keyboard.nextFloat();
 		System.out.println("Your weight in kg: ");
-		
+		userWeight = keyboard.nextFloat();
+		userBMI = userWeight / ( userHeight * userHeight);
+		System.out.println("\nYour BMI is " + userBMI);
+
 	}
 }
