5c9ab7af9f95f359e1241769958767f62fa61a0a
Michael "Cal" Krug <cal.krug@proton.me>
2022-08-29 01:50:45 -0500

    Add: Section 6 (If Statements 2) - 40-TheWorstNumberGuessingGameEver

--- 06-IfStatements2/40-TheWorstNumberGuessingGameEver/WorstGuessingGame.java
+++ 06-IfStatements2/40-TheWorstNumberGuessingGameEver/WorstGuessingGame.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class WorstGuessingGame {
   public static void main (String args[]) {
-    System.out.println("Let's get to this later");
+
+    Scanner kb = new Scanner(System.in);
+
+    // >that one line of code where the secret number is
+    byte numSecret = 7, numGuess;
+
+    System.out.println("The Worst Guessing Game\nGuess a number from one-to-ten!");
+    System.out.print("> ");
+    numGuess = kb.nextByte();
+    System.out.println();
+
+    // logical success determiner
+    if (numGuess == numSecret) {
+      System.out.println("Good job, it was " + numGuess + ". :)");
+    } else {
+      System.out.println("Got you! No, it was actually " + numGuess + ".");
+    }
+
   }
 }
