329a8881088084747737f59fc495d6677bd2953c
Michael "Cal" Krug <cal.krug@proton.me>
2022-08-29 10:41:38 -0500

    Add: Section 6 (If Statements 2) - 40-TheWorstNumberGuessingGameEver (Complete)

--- 07-RandomNumbers/41-Randomness/Randomness.java
+++ 07-RandomNumbers/41-Randomness/Randomness.java
@@ -4,7 +4,7 @@ public class Randomness {
   public static void main (String args[]) {
 
     // random generator object
-    Random r = new Random();
+    Random r = new Random(); // number in the parenthesis would be a "seed"
 
     // pick a random integer
     int x = 1 + r.nextInt(10);
@@ -12,12 +12,12 @@ public class Randomness {
     System.out.println("My random number is " + x);
 
     System.out.println("Here are some random numbers from 1 to 5!");
-    System.out.print(1 + r.nextInt(5) + " ");
-    System.out.print(1 + r.nextInt(5) + " ");
-    System.out.print(1 + r.nextInt(5) + " ");
-    System.out.print(1 + r.nextInt(5) + " ");
-    System.out.print(1 + r.nextInt(5) + " ");
-    System.out.print(1 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
+    System.out.print(3 + r.nextInt(5) + " ");
     System.out.println();
 
     System.out.println("Here are some random numbers from 1 to 100!");
@@ -43,10 +43,16 @@ public class Randomness {
     /* WHAT YOU SHOULD DO ON YOUR OWN
      * 
      * 1. The range for the new random numbers 0-4
-     * 2. The range for the new random numbers 3-8
-     * 3. TODO: Answer this question
-     * 4. TODO: Answer this question
-     * 5. TODO: Delete the seed
+     * 
+     * 2. The range for the new random numbers 3-7
+     * 
+     * 3. I notice that, when the seed is added, a particular set of numbers are printed each time,
+     * that's to say, the numbers are no longer random at all.
+     * 
+     * 4. The same thing as my answer to question three happens: a set sequence of numbers appear
+     * based on the seed placed as an argument on the Random object's instances.
+     * 
+     * 5. [Deleted the seed as per requirements of the assignment]
      */
 
   }
