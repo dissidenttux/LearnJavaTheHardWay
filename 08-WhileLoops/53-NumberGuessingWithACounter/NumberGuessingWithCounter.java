ae53880127849c770cbe694b8e4d7f83258f31a4
Michael "Cal" Krug <cal.krug@proton.me>
2022-09-04 11:11:26 -0500

    Add: Section 8 (While Loops), Assign. 53-NumberGuessingWithACounter

--- 08-WhileLoops/53-NumberGuessingWithACounter/NumberGuessingWithCounter.java
+++ 08-WhileLoops/53-NumberGuessingWithACounter/NumberGuessingWithCounter.java
@@ -8,16 +8,18 @@ public class NumberGuessingWithCounter {
     Random rand = new Random();
 
     // >that one line of code where the secret number is
-    int numSecret = 1 + rand.nextInt(10), numGuess;
+    int numSecret = 1 + rand.nextInt(10), numGuess, numGuesses = 0; //numGuess is the guessing number, numGuesses is the amount of times the user guesses the number
 
     System.out.println("The Worst Guessing Game: Keep Guessing!\nGuess a number from one-to-ten!");
     System.out.print("> ");
     numGuess = kb.nextByte();
+    numGuesses++; // first guess
     System.out.println();
 
     // logical success determiner
 
     while (numGuess != numSecret) {
+      numGuesses++; // subsequent guesses
       System.out.println("Incorrect. Let's see you guess again..");
       System.out.print("> ");
       numGuess = kb.nextByte();
@@ -25,6 +27,7 @@ public class NumberGuessingWithCounter {
     }
 
     System.out.println("Yes! You got it correct!");
+    System.out.println("It took you " + numGuesses + " guesses.");
 
   }
 }
